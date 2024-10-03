61788ccf3467c059f1cce2c9ea41a4bcc0de3e5e
Michael J. <86041862+Michael0131@users.noreply.github.com>
2024-10-03 13:23:42 -0600

    this is the final set up

---
+++ DS250_Project_Files/Project4/project_coding.py
@@ -0,0 +1,16 @@
+"""
+This file is here if you want to do your coding for the project in a Python file
+before you copy it to the QMD file
+"""
+
+import pandas as pd
+import numpy as np
+from lets_plot import *
+LetsPlot.setup_html()
+
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+# Load any data you need for the report
+# df = pd.read_csv('filename.csv')
+
