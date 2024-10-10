19eca732b7b4506fc499695ad58fd6b1bdd03817
Michael0131 <michaelj0131@byui.edu>
2024-10-09 19:16:19 -0600

    this is a working solution, could turn it in

--- DS250_Project_Files/Project1/project_coding.py
+++ DS250_Project_Files/Project1/project_coding.py
@@ -2,7 +2,7 @@
 This file is here if you want to do your coding for the project in a Python file
 before you copy it to the QMD file
 """
-
+from skimpy import clean_columns
 import pandas as pd
 import numpy as np
 from lets_plot import *
@@ -11,6 +11,19 @@ LetsPlot.setup_html()
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
-# Load any data you need for the report
-# df = pd.read_csv('filename.csv')
+from lets_plot import *
+LetsPlot.setup_html()
 
+# Sample data
+data = {'x': [1, 2, 3, 4, 5], 'y': [10, 15, 13, 17, 9]}
+
+# Create the plot using ggplot
+plot = ggplot(data) + geom_line(aes(x='x', y='y'))
+
+# Save the plot as an HTML file
+plot.save_as_html(filename="ds250_project_1.html")
+
+print("HTML file has been generated: ds250_project_1.html")
+
+
+print('This code is running')
