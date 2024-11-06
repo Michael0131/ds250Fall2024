a8105ce8f9532fc3aed1b64df68ca8f33fcb31db
Michael J. <86041862+Michael0131@users.noreply.github.com>
2024-11-05 17:05:53 -0700

    task 2 done

--- DS250_Project_Files/Project3/project_coding.py
+++ DS250_Project_Files/Project3/project_coding.py
@@ -7,9 +7,22 @@ import pandas as pd
 import numpy as np
 from lets_plot import *
 LetsPlot.setup_html()
+import sqlite3
 
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
+
+
+con = sqlite3.connect('DS250_Project_Files\\Project3\\lahmansbaseballdb.sqlite')
+# Write the SQL query
+
+query = "SELECT CAST(H AS FLOAT) / AB AS batting_average FROM batting WHERE playerID = 'addybo01' AND yearID = '1871'"
+df = pd.read_sql_query(query, con)
+
+print(df.round(3))
+# we use player ID: addybo01
+# then we need to get the AB in batting
+
 
 # Load any data you need for the report
 # df = pd.read_csv('filename.csv')
